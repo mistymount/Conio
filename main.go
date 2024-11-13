00c513ffcae8f885d402ca9b9da72ce4233bd10c
Satyam Singh <crackcoder@outlook.com>
2024-11-13 09:09:33 +0530

    Configure cockroach url

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package conio
 
 import "fmt"
 
+const (
+	databaseURL = "postgresql://root@127.0.0.1:26257/conio?sslmode=disable"
+	port        = 3000
+)
+
 func main() {
 	fmt.Println("Conio - Your personal secure social network")
 }
