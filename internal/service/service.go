6568f9077dbef159c80454660b8419a7cb3af606
Satyam Singh <crackcoder@outlook.com>
2024-11-13 09:50:18 +0530

    Setting up main

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,10 +2,18 @@ package service
 
 import (
 	"database/sql"
+
 	"github.com/hako/branca"
 )
 
 type Service struct {
 	db    *sql.DB
 	codec *branca.Branca
+}
+
+func New(db *sql.DB, codec *branca.Branca) *Service {
+	return &Service{
+		db:    db,
+		codec: codec,
+	}
 }
--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package conio
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	"github.com/hako/branca"
+	"github.com/mistymount/Conio/internal/service"
+)
 
 const (
 	databaseURL = "postgresql://root@127.0.0.1:26257/conio?sslmode=disable"
@@ -9,4 +16,18 @@ const (
 
 func main() {
 	fmt.Println("Conio - Your personal secure social network")
+	db, err := sql.Open("pgx", databaseURL)
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v\n", err)
+		return
+	}
+
+	defer db.Close()
+	if err := db.Ping(); err != nil {
+		log.Fatalf("Error pinging database: %v\n", err)
+		return
+	}
+
+	codec := branca.NewBranca("supersecretkeywhichissecureandencrypted")
+	service.New(db, codec)
 }
